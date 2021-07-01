e12617adf9a53075c64d623b58b66c7be315a450
khoidd <khoidd@outlook.com>
2021-07-01 14:22:09 +0700

    add react-webcam

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import MPose from '@mediapipe/pose';
+import MDrawing from '@mediapipe/drawing_utils';
+import MCamera from '@mediapipe/camera_utils';
+import MControl from '@mediapipe/control_utils';
 import './App.css';
 
 const App: React.FC = () => {
   const pose: MPose.Pose = new MPose.Pose({
     locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.4.1624666670/${file}`
   });
-
-  console.log(pose)
+  
+  
   return (
     <p>Hi</p>
   );
