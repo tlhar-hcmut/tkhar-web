0a6cef94a85a6f922801a17785a5bfe0a0c83716
khoidd <khoidd@outlook.com>
2021-07-04 19:14:11 +0700

    sent data func

--- src/pages/DemoUpload/index.tsx
+++ src/pages/DemoUpload/index.tsx
@@ -1,14 +1,18 @@
 import { UploadOutlined } from '@ant-design/icons';
-import MPose, { InputImage } from '@mediapipe/pose';
+import MPose, { NormalizedLandmarkList } from '@mediapipe/pose';
 import { Button, Col, Row, Upload } from 'antd';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
+const lsSkeleton: NormalizedLandmarkList[] = []
 
 const App: React.FC = () => {
-    const [video, setVideo] = useState<HTMLVideoElement | null>();
-    const [videoFile, setVideoFile] = useState<File>();
+    const [urlVideo, setUrlVideo] = useState<string>();
+    const [refVideo, setRefVideo] = useState<HTMLVideoElement | null>();
 
     useEffect(() => {
+        if (!refVideo) return;
+
         const path = "https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.4.1624666670/"
         const pose: MPose.Pose = new MPose.Pose({ locateFile: (file) => `${path}/${file}` })
 
@@ -19,30 +23,42 @@ const App: React.FC = () => {
             minTrackingConfidence: 0.5,
         })
 
-        pose.onResults((results) => {
-            console.log(results)
-        })
+        pose.onResults((results) => { lsSkeleton.push(results.poseWorldLandmarks) })
 
-        if (video) pose.send({ image: video as InputImage })
-    })
+        refVideo.addEventListener("play", async () => {
+            while (true) {
+                if (refVideo.paused || refVideo.ended) {
+                    axios.post("http://localhost:8000/har", { data: lsSkeleton })
+                        .then((res) => console.info(res))
+                        .catch((err) => console.error(err))
+                    lsSkeleton.length = 0
+                    break
+                };
+                pose.send({ image: refVideo })
+                await new Promise(r => setTimeout(r, 1000));
+            }
+
+        }, false)
+
+    }, [refVideo])
     return <Row>
         <Col span={8}>
             <Upload
                 maxCount={1}
                 customRequest={({ file, onSuccess }) => {
-                    setVideoFile((file as File));
+                    setUrlVideo(URL.createObjectURL(file as File));
                     if (onSuccess) onSuccess("ok", new XMLHttpRequest());
                 }}
-                onRemove={() => setVideoFile(undefined)}
+                onRemove={() => setUrlVideo(undefined)}
             >
                 Add video at here, and see your result <Button icon={<UploadOutlined />}>Upload</Button>
             </Upload>
         </Col>
         <Col span={8}>
-            {videoFile ? <video ref={setVideo} width="400" controls><source src={URL.createObjectURL(videoFile)} /></video> : <></>}
+            {urlVideo ? <video ref={setRefVideo} width="400" controls><source src={urlVideo} /></video> : <></>}
         </Col>
         <Col span={8}>
-            <p>Skeleton will show here!</p>
+
         </Col >
     </Row >
 }
