e2ee529b9f1b5968a86a2444934648d822757a24
khoidd <khoidd@outlook.com>
2021-08-06 22:49:19 +0700

    update code

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -aurv . tkhar@20.205.205.211:/home/tkhar/tkhar-web --exclude='node_modules'
+sshpass -p "Aa@123456789" rsync -aurv . tkhar@20.205.205.211:/home/tkhar/tkhar-web --exclude='node_modules'
