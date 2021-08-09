006fa692a05951fabc76c96a57d0c9496ed4fb45
thucth <thucth@vng.com.vn>
2021-08-09 16:00:16 +0700

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { Collapse } from 'antd';
-import React, { useEffect } from 'react';
+import { Button, Collapse, Modal } from 'antd';
+import React, { useEffect, useState } from 'react';
 // import MediaPipeDemo from "src/pages/DemoMediaPipe";
 // import UploadSkeletonDemo from "src/pages/DemoUploadSkeleton";
 import UploadVideoDemo from "src/pages/DemoUploadVideo";
@@ -10,19 +10,84 @@ const App: React.FC = () => {
     document.title = "TK HAR"
   }, []);
 
-  return <>
-    <Collapse>
-      {/* <Collapse.Panel header="Mediapipe Demo" key="1">
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const handleAbout = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <div className="header">
+        <h1
+          style={{
+            textAlign: "center",
+            fontFamily: "Trocchi",
+            color: "#7c795d",
+            fontSize: "50px",
+            fontWeight: 400,
+          }}
+        >
+          Human Action Recognition Application
+        </h1>
+        <div className="header-right">
+          <Button className="active" href="">
+            Home
+          </Button>
+          <Button onClick={handleAbout}>About</Button>
+        </div>
+      </div>
+
+      <Collapse defaultActiveKey="3">
+        {/* <Collapse.Panel header="Mediapipe Demo" key="1" style={{fontSize:"15px"}}>
         <MediaPipeDemo />
       </Collapse.Panel>
-      <Collapse.Panel header="Upload Skeleton Demo" key="2">
+      <Collapse.Panel header="Upload Skeleton Demo" key="2" style={{fontSize:"15px"}}>
         <UploadSkeletonDemo />
       </Collapse.Panel> */}
-      <Collapse.Panel header="Upload Video Demo" key="3">
-        <UploadVideoDemo />
-      </Collapse.Panel>
-    </Collapse>
-  </>
+        <Collapse.Panel
+          header="Upload Video Demo"
+          key="3"
+          style={{ fontSize: "20px" }}
+        >
+          <UploadVideoDemo />
+        </Collapse.Panel>
+      </Collapse>
+
+      {/* Modal */}
+      <Modal
+        title="About HAR application"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        width={1000}
+      >
+        <p>Author: Tran Huu Thuc, Do Dang Khoi.</p>
+        <p>
+          Desciptions: This is a demonstration of our thesis topic{" "}
+          <b>Action recognition in video using machine learning</b>. In this
+          website, you can upload video and our model will predict action
+          performed in video.
+        </p>
+        <p>
+          Data: You can download videos on link (these videos are made ourselves):
+          <a
+            href="https://drive.google.com/drive/folders/1BRrJaN3NcO5ujfmO5AuRqjBYHyrQbw9c?usp=sharing"
+            target="_blank"
+          >
+            {"  "} video_actions.
+          </a>
+        </p>
+      </Modal>
+    </>
+  );
 
 }
 
--- src/pages/DemoUploadVideo/index.tsx
+++ src/pages/DemoUploadVideo/index.tsx
@@ -8,57 +8,73 @@ const App: React.FC = () => {
     const [output, setOutput] = useState<HarResponse>();
     const [urlVideo, setUrlVideo] = useState<string>();
 
-    return <Row>
+    return (
+      <Row>
         <Col span={12}>
-            <Upload
-                maxCount={1}
-                customRequest={({ file, onSuccess }) => {
-                    setUrlVideo(undefined)
-                    setOutput(undefined)
+          <Upload
+            maxCount={1}
+            customRequest={({ file, onSuccess }) => {
+              setUrlVideo(undefined);
+              setOutput(undefined);
 
-                    setUrlVideo(URL.createObjectURL(file as File));
-                    const formData = new FormData()
-                    formData.append("filename", "fucku")
-                    formData.append("file", file)
-                    axios.post("http://20.205.205.211:8000/video", formData)
-                        .then((res) => {
-                            setOutput(res.data)
-                            console.info(res);
-                        })
-                        .catch((err) => console.error(err))
-                    if (onSuccess) onSuccess("ok", new XMLHttpRequest());
-                }}
-                onRemove={() => {
-                    setUrlVideo(undefined)
-                    setOutput(undefined)
-                }}
-            >
-                Add video at here, and see your result <Button icon={<UploadOutlined />}>Upload</Button>
-            </Upload>
-            {urlVideo ? <video width="500" controls><source src={urlVideo} /></video> : <></>}
+              setUrlVideo(URL.createObjectURL(file as File));
+              const formData = new FormData();
+              formData.append("filename", "fucku");
+              formData.append("file", file);
+              axios
+                .post("http://20.205.205.211:8000/video", formData)
+                .then((res) => {
+                  setOutput(res.data);
+                  console.info(res);
+                })
+                .catch((err) => console.error(err));
+              if (onSuccess) onSuccess("ok", new XMLHttpRequest());
+            }}
+            onRemove={() => {
+              setUrlVideo(undefined);
+              setOutput(undefined);
+            }}
+          >
+            <span style={{ fontSize: "20px", padding:"10px" }} >
+              Add video at here, and see your result
+            </span>
+              <Button icon={<UploadOutlined />}>Upload</Button>
+          </Upload>
+          {urlVideo ? (
+            <video width="500" controls>
+              <source src={urlVideo} />
+            </video>
+          ) : (
+            <></>
+          )}
         </Col>
         <Col span={12}>
-            <Table pagination={{ pageSize: 12 }} dataSource={output?.data} columns={[
-                {
-                    title: 'ID',
-                    dataIndex: 'id',
-                    key: 'id',
-                },
-                {
-                    title: 'Action',
-                    dataIndex: 'action',
-                    key: 'action',
-                },
-                {
-                    title: 'Confidence (%)',
-                    dataIndex: 'confidence',
-                    key: 'confidence',
-                    sorter: (a: Action, b: Action) => a.confidence - b.confidence,
-                    defaultSortOrder: 'descend',
-                },
-            ]} />
+          <Table
+            pagination={{ pageSize: 12 }}
+            dataSource={output?.data}
+            columns={[
+              {
+                title: "ID",
+                dataIndex: "id",
+                key: "id",
+              },
+              {
+                title: "Action",
+                dataIndex: "action",
+                key: "action",
+              },
+              {
+                title: "Confidence (%)",
+                dataIndex: "confidence",
+                key: "confidence",
+                sorter: (a: Action, b: Action) => a.confidence - b.confidence,
+                defaultSortOrder: "descend",
+              },
+            ]}
+          />
         </Col>
-    </Row >
+      </Row>
+    );
 }
 
 export default App
